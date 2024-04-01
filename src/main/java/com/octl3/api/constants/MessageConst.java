55dbeac5a961e5da2bdf81a1a8aadbbc5c555a3f
ThaoLD <thaold48mta@gmail.com>
2024-04-01 09:44:05 +0700

    update method submit and delete

--- src/main/java/com/octl3/api/constants/MessageConst.java
+++ src/main/java/com/octl3/api/constants/MessageConst.java
@@ -2,4 +2,7 @@ package com.octl3.api.constants;
 
 public class MessageConst {
 
+    public static final String SUBMIT_SUCCESS = "Submit success!";
+    public static final String DELETE_SUCCESS = "Delete success!";
+
 }
